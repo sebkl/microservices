33239c9a4992f44d399fcb22f349a2a63e50cb67
Sebastian Klöppel <sk@nakedape.net>
2016-02-18 14:21:11 +0100

    Switched to upload URL storage instead of deprecated File API.

--- appengine/shareme/shareme.go
+++ appengine/shareme/shareme.go
@@ -1,13 +1,20 @@
 //package shareme offers an experimental google appengine example service which allows
-// to share files based on a hash key which can be distributed as an URL. It demonstrates 
-// the  use of the blobstore functionality in combination with the gotojs package for
+// to share files based on a hash key which can be distributed as an URL. It demonstrates
+// the  use of the blobstore and datastore functionality in combination with the gotojs package for
 // appengine integration.
 //
 // TODOS:
-//	- Check valid period by ornjob or on certain calls.
+//	- Fix duplicate datastore key issue
+//	- Make responsive
+// 	- use simple icons set for download, delete, share etc.
+//	- use simple mime-type icons
+//	- share button shloud copy the data url in the clipboard.
+//	- Remove code noise !
+// 	- create preview url ( adds the share to your session :-D)
+//	- Check valid period by cronjob or on certain calls.
+//	- Register escape button to unselect + ui button
 //	- make responsive for mobiles.
 //	- configurable file type and size limits on both, server and client side.
-//	- store meta information such as name,thumbnail in datastore
 //	- support multiple files
 //	- add send-to link (mailto:)
 //	- add copy button which copies link to clipboard (!)
@@ -15,65 +22,94 @@
 //	- consider authentication and authorization options per share.
 //	- add default thumbnail for non pictures/videos
 //	- implement/use url shortener (together with datastore)
+//	- make storage configurable
+
 package shareme
 
 import (
+	"appengine/datastore"
+	"appengine/user"
+	"encoding/json"
+	"fmt"
 	. "github.com/sebkl/gotojs"
 	"github.com/sebkl/gotojs/gae"
-	"github.com/sebkl/microservices/gotojs/imgsrv"
 	"github.com/sebkl/imgurl"
-	"log"
+	"github.com/sebkl/microservices/gotojs/imgsrv"
 	"io"
-	"appengine"
-	"appengine/blobstore"
-	"appengine/user"
-	"time"
-	"errors"
+	"log"
 	"net/http"
-	"fmt"
+	"net/url"
 	"strings"
+	"time"
 )
 
 const (
 	DefaultValidPeriod = 7 * 24 * 60 * 60 * 1000 //7days in msecs
-	Msec = 1000 * 1000
-	KeySessionPrefix = "key:"
-	InterfaceName = "Shareme"
+	Msec               = 1000 * 1000
+	KeySessionPrefix   = "key:"
+	InterfaceName      = "Shareme"
 )
 
+type StorageService interface {
+	UploadURL(c *gae.Context) (*url.URL, error)
+	Delete(c *gae.Context, key string) error
+	Stat(c *gae.Context, key string) (*Share, error)
+	Get(c *gae.Context, key string) (io.ReadCloser, error)
+	Thumbnail(c *gae.Context, key string, w, h int) string
+	Type() int
+	HandleUpload(*HTTPContext) []*Share
+}
+
 type Share struct {
-	Key string `json:"key,omitempty"`
-	ValidPeriod int64 `json:"valid_period,omitempty"` //msec
-	Size int64 `json:"size,omitempty"`
-	Name string `json:"name,omitempty"`
-	Created int64 `json:"created,omitempty"` //msec unix
-	MimeType string `json:"mimetype,omitempty"`
-	Error string `json:"error,omitempty"`
-	Url string `json:"url,omitempty"`
-	Thumbnail string `json:"thumbnail,omitempty"`
+	Key            string         `json:"key,omitempty" datastore:"-"`
+	DSK            *datastore.Key `json:"-" datastore:"-"`
+	StorageKey     string         `json:"-" datastore:"storagekey"`
+	Size           int64          `json:"size,omitempty" datastore:"size"`
+	Name           string         `json:"name,omitempty" datastore:"name"`
+	Created        int64          `json:"created,omitempty" datastore:"created"` //msec unix
+	Expires        int64          `json:"expires,omitempty" datastore:"expires"` //msec unix
+	MimeType       string         `json:"mimetype,omitempty" datastore:"mimetype"`
+	Error          string         `json:"error,omitempty" datastore:"-"`
+	Url            string         `json:"url,omitempty" datastore:"-"`
+	Thumbnail      string         `json:"thumbnail,omitempty" datastore:"-"`
+	StorageService StorageService `json:"-" datastore:"-"`
+	StorageType    int            `json:"storage,omitempty" datastore:"storagetype"`
+}
+
+func ReadShare(r io.Reader) *Share {
+	dec := json.NewDecoder(r)
+	s := &Share{}
+	err := dec.Decode(s)
+	if err != nil {
+		log.Printf("Could not decode share: %s", err)
+		return nil
+	}
+	return s
 }
 
 func (s *Share) IsError() bool {
 	return len(s.Error) > 0
 }
 
-func (s *SharemeService) newShare(gae *gae.GAEContext, bi *blobstore.BlobInfo) Share {
-	return Share{
-		Key: string(bi.BlobKey),
-		ValidPeriod: DefaultValidPeriod - int64(time.Now().Sub(bi.CreationTime).Nanoseconds() / Msec),
-		Size: bi.Size,
-		MimeType: bi.ContentType,
-		Created: int64(bi.CreationTime.UnixNano() / Msec),
-		Url: fmt.Sprintf("%s/%s/%s/%s",gae.HTTPContext.Frontend.BaseUrl(),InterfaceName,"Get",string(bi.BlobKey)),
-		Thumbnail: s.ThumbnailURL(gae,string(bi.BlobKey)),
-	}
+func (s *Share) WriteTo(w io.Writer) error {
+	enc := json.NewEncoder(w)
+	return enc.Encode(s)
 }
 
-func noShare(err error) Share {
-	return Share{
-		Error: err.Error(),
-		Size: 0,
-		ValidPeriod: 0,
+func (s *Share) String() string {
+	by, err := json.Marshal(s)
+	if err != nil {
+		return ""
+	}
+	return string(by)
+}
+
+func noShare(err error) *Share {
+	log.Printf("Could generate share object: %s", err)
+	return &Share{
+		Error:   err.Error(),
+		Size:    0,
+		Expires: 0,
 	}
 }
 
@@ -90,52 +126,21 @@ func (b *BlobBinary) Close() error {
 	return nil
 }
 
+// TODO: Make actual storage configurable (Cloud storage, blobstore etc ...)
 type SharemeService struct {
-}
-
-func (s *SharemeService) Get(c* gae.GAEContext, key string) interface{} { // Return either Share in case of Error or Binary.
-	k := appengine.BlobKey(key)
-	bi,err := blobstore.Stat(c,k)
-	if err != nil {
-		c.HTTPContext.ReturnStatus = http.StatusNotFound
-		return noShare(err)
-	}
-
-	reader :=blobstore.NewReader(c,k)
-	//TODO: set Content-Disposition.
-	// If exisits, use from session, otherwise, something like download.<mime-type-ending>	
-	return &BlobBinary{Reader: reader, mimeType: bi.ContentType}
-}
-
-func (s *SharemeService) Stat(c *gae.GAEContext, key string) Share {
-	bi,err := blobstore.Stat(c,appengine.BlobKey(key))
-	if err != nil {
-		c.HTTPContext.ReturnStatus = http.StatusNotFound
-		return noShare(err)
-	} else {
-		return s.newShare(c,bi)
-	}
+	storageService StorageService
 }
 
 //Thumbnail returns a 50x50 thumbnail data url of a key if possible.
-func (s *SharemeService) ThumbnailURL(c *gae.GAEContext, key string) string {
-	//TODO: check cache (meta info in data store)
-	obj := s.Get(c,key)
-	if bb,ok := obj.(*BlobBinary); ok && strings.HasPrefix(bb.MimeType(),"image") {
-		if url,_,err := imgurl.UrlifyR(bb,bb.MimeType(), 50, 50); err == nil {
-			return url
-		} else {
-			log.Printf("Could urlify blob: %s",err)
-		}
-	}
-	return ""
+func (s *SharemeService) ThumbnailURL(c *gae.Context, key string) string {
+	return s.Stat(c, key).Thumbnail
 }
 
 //ImageURL returns the given object das a data url.
-func (s *SharemeService) ImageURL(c *gae.GAEContext, key string) string {
-	obj := s.Get(c,key)
-	if bb,ok := obj.(*BlobBinary); ok && strings.HasPrefix(bb.MimeType(),"image") {
-		if url,_,err := imgurl.UrlifyR(bb,bb.MimeType(),0,0); err == nil {
+func (s *SharemeService) ImageURL(c *gae.Context, key string) string {
+	obj := s.Get(c, key)
+	if bb, ok := obj.(*BlobBinary); ok && strings.HasPrefix(bb.MimeType(), "image") {
+		if url, _, err := imgurl.UrlifyR(bb, bb.MimeType(), 0, 0); err == nil {
 			return url
 		} else {
 			panic(err)
@@ -145,80 +150,178 @@ func (s *SharemeService) ImageURL(c *gae.GAEContext, key string) string {
 }
 
 //MyShares returns a list of all shares that have been upload by the user.
-func (s *SharemeService) MyShares(c *gae.GAEContext, session *Session) []Share {
-	ret := make([]Share,0,len(session.Properties))
-	for k,name := range session.Properties {
-		if strings.HasPrefix(k,KeySessionPrefix) {
-			key := strings.TrimLeft(k,KeySessionPrefix)
-			share := s.Stat(c,key)
+func (s *SharemeService) MyShares(c *gae.Context, session *Session) []Share {
+	ret := make([]Share, 0, len(session.Properties))
+	for k, name := range session.Properties {
+		if strings.HasPrefix(k, KeySessionPrefix) {
+			key := strings.TrimLeft(k, KeySessionPrefix)
+			share := s.Stat(c, key)
+
 			share.Name = name
 			if !share.IsError() {
-				ret = append(ret,share)
+				ret = append(ret, share)
+			} else {
+				//If there is an error with key, delete it from session.
+				tdk := KeySessionPrefix + key
+				session.Delete(tdk)
 			}
 		}
+	}
+	c.HTTPContext.ReturnStatus = http.StatusOK
+	return ret
+}
+
+func (s *Share) Delete(c *gae.Context) (err error) {
+	err = s.StorageService.Delete(c, s.StorageKey)
+	if err != nil {
+		return
+	}
+	err = datastore.Delete(c, s.DSK)
+	return err
+}
+
+// Delete removes the object of a key from the storage. It wont be accessible afterwards.
+func (s *SharemeService) Delete(c *gae.Context, session *Session, key string) (ret Share) {
+	stat := s.Stat(c, key)
+	if stat.IsError() {
+		return stat
+	}
+
+	err := stat.Delete(c)
+	if err != nil {
+		return *noShare(err)
+	}
+
+	c.HTTPContext.ReturnStatus = http.StatusOK
+	session.Delete(fmt.Sprintf("%s%s", KeySessionPrefix, key))
+	ret = Share{Key: ret.Key, Expires: 0, Size: 0}
+	return
+}
+
+// Upload URL generates an URL to be used for direct uploading of a blob.
+func (s *SharemeService) UploadURL(c *gae.Context, cont *Container) string {
+	url, err := s.storageService.UploadURL(c)
+	log.Printf("Generated url :'%s'", url.String())
+	if err != nil {
+		log.Printf("Could not generate UpladURL %s", err)
+	}
+
+	return url.String()
+}
+
+//Get returns the blob itself.
+func (s *SharemeService) Get(c *gae.Context, key string) interface{} { // Return either Share in case of Error or Binary.
+	st := s.Stat(c, key)
+	if st.IsError() {
+		return noShare(fmt.Errorf(st.Error))
+	}
+
+	/* retrieve blob */
+	rc, err := s.storageService.Get(c, st.StorageKey)
+	if err != nil {
+		c.HTTPContext.ReturnStatus = http.StatusNotFound
+		//TODO: delete key from datastore and session.
+		return noShare(err)
+	}
+	/* generate return object */
+	return &BlobBinary{Reader: rc, mimeType: st.MimeType}
+}
+
+//Add adds a key to the users session and returns its descriptor.
+func (s *SharemeService) Add(c *gae.Context, session *Session, key string) (stat Share) {
+	stat = s.Stat(c, key)
+	if stat.IsError() {
+		return
+	}
+	session.Set(fmt.Sprintf("%s%s", KeySessionPrefix, key), stat.Name)
+	return
+}
+
+//Stat returns the descriptor of the share.
+func (s *SharemeService) Stat(c *gae.Context, key string) Share {
+	dsk, err := datastore.DecodeKey(key)
+	if err != nil {
+		c.HTTPContext.ReturnStatus = http.StatusNotFound
+		return *noShare(fmt.Errorf("Error decoding key '%s': %s", key, err))
+	}
+	stat := &Share{DSK: dsk}
+	err = datastore.Get(c, dsk, stat)
+	switch err {
+	case datastore.ErrNoSuchEntity:
+		c.HTTPContext.ReturnStatus = http.StatusNotFound
+		return *noShare(fmt.Errorf("Entity not found for key '%s' : %s", key, err))
+	default:
+		c.HTTPContext.ReturnStatus = http.StatusInternalServerError
+		return *noShare(fmt.Errorf("Error retrieving data for key '%s' : %s", key, err))
+	case nil:
+		share, err := s.storageService.Stat(c, stat.StorageKey)
+		if err != nil || len(share.StorageKey) <= 0 {
+			//TODO: delete from datastore
+			return *noShare(fmt.Errorf("No storage key for '%s' %s", key, err))
+		}
+		stat.Key = key
+		stat.Url = fmt.Sprintf("%s/%s/%s/%s", c.HTTPContext.Container.BaseUrl(), InterfaceName, "Get", key)
+		stat.Thumbnail = s.storageService.Thumbnail(c, stat.StorageKey, 50, 50)
+		stat.StorageService = s.storageService
+		return *stat
+	}
+}
+
+//HandleUpload is a method that deals with full form submits. Use the url of this binding as POST action for the
+//embedded Form
+func (s *SharemeService) HandleUpload(hc *HTTPContext, c *gae.Context, session *Session) []*Share {
+	ret := make([]*Share, 0)
+	bis := s.storageService.HandleUpload(hc)
+	for _, stat := range bis {
+		dsk, err := datastore.Put(c, datastore.NewIncompleteKey(c, "Share", nil), stat)
+		if err != nil {
+			//TODO: delete blob
+			continue
+		}
+		fkey := dsk.Encode()
+		session.Set(fmt.Sprintf("%s%s", KeySessionPrefix, fkey), stat.Name)
+		rstat := s.Stat(c, fkey)
+		ret = append(ret, &rstat)
+
 	}
 	return ret
 }
 
-// Delete removes the object of a key from the storage. It wont be accessible afterwards.
-func (s *SharemeService) Delete(c *gae.GAEContext, session *Session, key string) (ret Share) {
-	ret = s.Stat(c,key)
-	if ret.IsError() {
+// HandleCleanup removes all outdated elements based on their validity period.
+func HandleCleanup(w http.ResponseWriter, r *http.Request) {
+	hc := NewHTTPContext(r, w) // HTTPContext
+	c := gae.NewContext(hc)    //GAE Context
+	stamp := int64(time.Now().UnixNano() / Msec)
+	q := datastore.NewQuery("Share").Filter("expires >", stamp)
+
+	var shares []Share
+	keys, err := q.GetAll(c, &shares)
+	if err != nil {
+		log.Printf("Error during cleanip: %s", err)
 		return
 	}
 
-	session.Delete(fmt.Sprintf("%s%s",KeySessionPrefix,key))
-	if err := blobstore.Delete(c,appengine.BlobKey(key)); err != nil {
-		ret =  noShare(err)
-	} else {
-		ret = Share{Key: ret.Key,ValidPeriod: 0, Size: 0}
-	}
-	return
-}
-
-//Put creates a new share within the storage.
-func (s *SharemeService) Put(c *gae.GAEContext, session *Session, name string, bc *BinaryContent) (blob Share) {
-	if bc == nil {
-		return noShare(errors.New("No object provided."))
-	} else {
-		//user := user.Current(c)
-
-		writer, err := blobstore.Create(c, bc.MimeType())
-		if err != nil {
-			return noShare(err)
+	if len(keys) > 0 {
+		log.Printf("Found %d expired shares", len(shares))
+		for i, key := range keys {
+			shares[i].DSK = key
+			shares[i].Delete(c)
+			log.Printf("Share '%s' deleted", shares[i].Key)
 		}
-
-		bi,err := io.Copy(writer,bc);
-		if err != nil {
-			return noShare(err)
-		}
-
-		writer.Close()
-		bc.Close()
-
-		key, err := writer.Key()
-		if err != nil {
-			return noShare(err)
-		}
-
-		session.Set(fmt.Sprintf("%s%s",KeySessionPrefix,key),name)
-
-		log.Printf("BLOBSTORE created %d bytes [%s]",bi,key)
-		stat := s.Stat(c,string(key))
-		stat.Name = name
-		return stat
 	}
 }
 
-//NewSharemeService create a new service instance.
-func NewSharemeService() (ret *SharemeService) {
-	ret = &SharemeService{}
+//NewSharemeService creates a new service instance.
+func NewSharemeService(ak ...string) (ret *SharemeService) {
+	ret = &SharemeService{
+		storageService: &BlobstoreStorageService{},
+	}
 	return
 }
 
 //RestrictToAdmin is a GOTOJS filter that allow to restrict certain calls to the administrators
-// of the appengine module/app
-func RestrictToAdmin(c *gae.GAEContext) bool {
+// of the app engine module/app
+func RestrictToAdmin(c *gae.Context) bool {
 	if c == nil {
 		c.HTTPContext.ReturnStatus = http.StatusInternalServerError
 		log.Printf("Could not retrieve context.")
@@ -233,16 +336,24 @@ func RestrictToAdmin(c *gae.GAEContext) bool {
 	}
 }
 
-//init initializes the appengine module.
+//init initializes the app engine module.
 func init() {
 	log.Printf("INIT()")
-	f := NewFrontend()
-	f.Redirect("/","/p/")
-	f.ExposeInterface(NewSharemeService(),InterfaceName)
-	f.ExposeInterface(&imgsrv.ImageService{},"Image")
-	f.EnableFileServer("htdocs","p")
-	log.Printf("%s",f.InterfaceNames())
-	gae.SetupAndStart(f)
-	//f.Bindings().Match("(Put|Delete)$").If(AutoInjectF(RestrictToAdmin))
-}
+	// This key must be the same across all GAE instances in order to decrypt session data
+	c := NewContainer(Properties{P_APPLICATIONKEY: "APPKEY_CHANGEME_________________"})
 
+	c.Redirect("/", "/p/")
+
+	sms := NewSharemeService()
+	c.ExposeInterface(sms, InterfaceName)
+	c.ExposeInterface(&imgsrv.ImageService{}, "Image")
+	c.EnableFileServer("htdocs", "p")
+
+	ub, _ := c.Binding(InterfaceName, "HandleUpload")
+	sms.storageService = &BlobstoreStorageService{ub.Url().Path}
+
+	c.ExposeYourself()
+
+	gae.SetupAndStart(c)
+	http.HandleFunc("/cleanup", HandleCleanup)
+}
--- appengine/shareme/storage.go
+++ appengine/shareme/storage.go
@@ -1,184 +1,99 @@
 package shareme
 
-
 import (
-	. "github.com/sebkl/gotojs"
-	"github.com/sebkl/gotojs/gae"
-	"log"
-	"io"
 	"appengine"
 	"appengine/blobstore"
-	"fmt"
-	"net/http"
-	"io/ioutil"
-	"bytes"
-	"mime/multipart"
+	. "github.com/sebkl/gotojs"
+	"github.com/sebkl/gotojs/gae"
 	"github.com/sebkl/imgurl"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/url"
 )
 
 const (
-	FileAPI = iota
-	UploadURL
+	BlobstoreStorageType = iota
 )
 
-
-type BlobstoreStorageService struct {}
-type BlobstoreUploadURL struct {
-	BlobstoreStorageService
-}
-type BlobstoreFileAPI struct {
-	BlobstoreStorageService
+type BlobstoreStorageService struct {
+	UploadURLPrefix string
 }
 
-func (st *BlobstoreStorageService) Thumbnail(c *gae.Context, key string, w,h int) (ret string) {
-	stat, err := st.Stat(c,key)
+func (st *BlobstoreStorageService) Thumbnail(c *gae.Context, key string, w, h int) (ret string) {
+	stat, err := st.Stat(c, key)
 	if err != nil {
-		log.Printf("Blobstore.Thumbnail '%s': %s",key,err)
+		log.Printf("Blobstore.Thumbnail '%s': %s", key, err)
 		return
 	}
 
-	reader, err := st.Get(c,key)
+	reader, err := st.Get(c, key)
 	if err != nil {
-		log.Printf("Blobstore.Thumbnail '%s': %s",key,err)
+		log.Printf("Blobstore.Thumbnail '%s': %s", key, err)
 		return
 	}
 
-	ret,_,err = imgurl.UrlifyR(reader,stat.MimeType,w,h)
+	ret, _, err = imgurl.UrlifyR(reader, stat.MimeType, w, h)
 	return
 }
 
 func (st *BlobstoreStorageService) Get(c *gae.Context, key string) (ret io.ReadCloser, err error) {
 	k := appengine.BlobKey(key)
-	_,err = blobstore.Stat(c,k)
+	_, err = blobstore.Stat(c, k)
 	if err == nil {
-		ret = ioutil.NopCloser(blobstore.NewReader(c,k))
+		ret = ioutil.NopCloser(blobstore.NewReader(c, k))
 	}
 	return
 }
 
-func (st *BlobstoreStorageService) Stat(c *gae.Context, key string) (*Share,error) {
-	bi,err := blobstore.Stat(c,appengine.BlobKey(key))
+func (st *BlobstoreStorageService) Stat(c *gae.Context, key string) (*Share, error) {
+	bi, err := blobstore.Stat(c, appengine.BlobKey(key))
+	if err != nil {
+		return nil, err
+	}
 	stat := newShare(bi)
-	return stat,err
+	return stat, err
 }
 
-func (st *BlobstoreStorageService) Delete(c *gae.Context,key string) (err error) {
-	_,err = st.Stat(c,key)
+func (st *BlobstoreStorageService) Delete(c *gae.Context, key string) (err error) {
+	_, err = st.Stat(c, key)
 	if err != nil {
 		return
 	}
 
-	err = blobstore.Delete(c,appengine.BlobKey(key))
+	err = blobstore.Delete(c, appengine.BlobKey(key))
 	return
 }
 
-
-func (st *BlobstoreFileAPI) Put(s *SharemeService,c *gae.Context, bc *BinaryContent, name string) (key string, err error) {
-	// ------------------------------------------------------------
-	// Thanks google for removing the File API. I'll keep the below
-	// code to remember good times.
-	// TODO: Remove once File api has been removed.
-	writer, err := blobstore.Create(c, bc.MimeType())
-	if err != nil {
-		return
-	}
-
-	bi,err := io.Copy(writer,bc);
-	if err != nil {
-		return
-	}
-
-	writer.Close()
-	bc.Close()
-
-	bkey, err := writer.Key()
-	key = string(bkey)
-	log.Printf("BLOBSTORE created %d bytes [%s] (using FileAPI)",bi,key)
-	return
+func (st *BlobstoreStorageService) UploadURL(c *gae.Context) (url *url.URL, err error) {
+	return blobstore.UploadURL(c, st.UploadURLPrefix, nil)
 }
 
-func (st *BlobstoreFileAPI) Type() int { return FileAPI }
+func (st *BlobstoreStorageService) Type() int { return BlobstoreStorageType }
 
-func (st *BlobstoreUploadURL) Put(s *SharemeService,c *gae.Context,bc *BinaryContent, name string) (key string, err error) {
-	//user := user.Current(c)
-
-	// ------------------------------------------------------------
-	// Beware of ugliness !!!
-	// Build upload URL, and call it ourselves. Workaround for 
-	// deprecated file API
-	bdry := Encoding.EncodeToString(GenerateKey(10)) // Generate random boundary.
-	url, err := blobstore.UploadURL(c, s.uploadURL(), nil)
+func (st *BlobstoreStorageService) HandleUpload(hc *HTTPContext) (ret []*Share) {
+	ret = make([]*Share, 0)
+	blobs, _, err := blobstore.ParseUpload(hc.Request)
 	if err != nil {
-		return
+		log.Printf("StorageService: Could not parse Upload: %s", err)
 	}
 
-	req,err  := http.NewRequest("POST",url.String(),nil)
-
-	req.Header.Set("Content-Type","multipart/form-data; boundary=" + bdry)
-
-	tb := bytes.NewBufferString("")
-	wr:= multipart.NewWriter(tb)
-	wr.SetBoundary(bdry)
-
-	mimeheader := make(map[string][]string)
-	mimeheader["Content-Type"] = []string{bc.MimeType()}
-	mimeheader["Content-Disposition"] = []string{"form-data; name=\"content\"; filename=\"" + name + "\""}
-	bcwr,err := wr.CreatePart(mimeheader)
-	bi, err := io.Copy(bcwr,bc)
-	if err != nil {
-		return
+	for _, bia := range blobs {
+		for _, bi := range bia {
+			ret = append(ret, newShare(bi))
+		}
 	}
-
-	err = wr.Close()
-	req.Body = ioutil.NopCloser(tb)
-	//req.Header.Set("shareme-auth":
-
-	resp, err := c.Client.Do(req)
-	if err != nil {
-		return
-	}
-	defer resp.Body.Close()
-
-	sh := ReadShare(resp.Body)
-	if sh == nil {
-		err = fmt.Errorf("Could not read share from internal upload url.")
-		return
-	}
-
-	key = sh.Key
-	log.Printf("BLOBSTORE created %d bytes [%s] (using UploadURL)",bi,key)
 	return
 }
-
-func (st *BlobstoreUploadURL) Type() int { return UploadURL }
-
 
 func newShare(bi *blobstore.BlobInfo) *Share {
 	ret := &Share{
-		Key: string(bi.BlobKey),
-		Expires: DefaultValidPeriod + int64(bi.CreationTime.UnixNano() / Msec),
-		Size: bi.Size,
-		MimeType: bi.ContentType,
-		Name: bi.Filename,
-		Created: int64(bi.CreationTime.UnixNano() / Msec),
+		StorageKey: string(bi.BlobKey),
+		Expires:    DefaultValidPeriod + int64(bi.CreationTime.UnixNano()/Msec),
+		Size:       bi.Size,
+		MimeType:   bi.ContentType,
+		Name:       bi.Filename,
+		Created:    int64(bi.CreationTime.UnixNano() / Msec),
 	}
 	return ret
-}
-
-// HandleUpload internally handles uploads
-// TODO: Do some kind of authentication here
-func HandleUpload(w http.ResponseWriter, r *http.Request)  {
-	//c := appengine.NewContext(r)
-	blobs, _, _ := blobstore.ParseUpload(r)
-
-	// TODO: Deal with multiple blobs
-	for _,b := range blobs {
-		s := newShare(b[0])
-		w.Header().Set("Content-Type","application/json")
-		s.WriteTo(w)
-		return
-	}
-
-	w.Header().Set("Content-Type","text/plain")
-	return
 }
